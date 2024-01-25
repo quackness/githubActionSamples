f0dd75edf15e734e47865e92f7618b91359459f0
Karolina Redden <dubajkaro@gmail.com>
2024-01-25 13:32:09 -0400

    add initial files

---
+++ src/main/java/com/nscc/fullstk/actionsdemo/ActionsdemoApplication.java
@@ -0,0 +1,13 @@
+package com.nscc.fullstk.actionsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActionsdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActionsdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nscc/fullstk/actionsdemo/ActionsdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nscc.fullstk.actionsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActionsdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
